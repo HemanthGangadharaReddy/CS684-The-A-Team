78f42bfbe9475f93ac53253b834a75a6d4c56b58
ven-zengeek <36300962+ven-zengeek@users.noreply.github.com>
2023-02-26 20:20:23 -0500

    Add files via upload

---
+++ login and sign up/process.php
@@ -0,0 +1,32 @@
+<?php
+
+// Connect to the database
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Sanitize form data
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+
+// Insert form data into the database
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
