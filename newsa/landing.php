826a52ef223f482778846cf4bd1aa10dcfa7acc2
ayushcham <99756116+ayushcham@users.noreply.github.com>
2023-02-26 19:21:54 -0500

    Landing Page php, style css, logo

---
+++ newsa/landing.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Newsa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+            <img src="media/logo.png" class="logo">
+        <ul class="nav-links">
+            <li><a href="">SPORTS</a></li>
+            <li><a href="">POLITICS</a></li>
+            <li><a href="">SCIENCE</a></li>
+            <li><a href="">HEALTH</a></li>
+            <li><a href="">ENTERTAINMENT</a></li>
+            <li><a href="">TECHNOLOGY</a></li>
+        </ul>
+        <button style="margin-left: 35px;" class="button">SIGN IN</button>
+        <button style="margin-left: 20px;" class="button">SIGN UP</button>
+       
+    </div>
+<div class="mid">WELCOME</div>
+</body>
+    </html>
